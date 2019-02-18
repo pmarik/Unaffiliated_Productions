61837bc7c0dc67437ab9e957553228c901e48da6
pmarik <pmarik@uwalumni.com>
2019-02-18 11:07:50 -0600

    updated pages with original dynamic menu

--- src/app.js
+++ src/app.js
@@ -1,16 +1,34 @@
 "use strict";
-import greet from "./testScript-1";
 
 
-console.log(greet);
 
+/**********************************
+ * Nav resize on scroll
+ * 
+ * 
+ * *******************************/
+
+
+
+
+//toggle animated nav when scrolling vertically
+let scrollHeight;
+let animatedHeader = document.getElementsByTagName('header')[0];
+const checkVideo = document.getElementsByClassName('wrapper')[0].childNodes;
+let videoHeight = 0;
+if (checkVideo[1].className == "video-wrap") {
+    videoHeight = document.getElementsByClassName('video-wrap')[0].clientHeight;
+}
+
+
+videoHeight -= 70;
+//readjust scroll measurement when resizing window for nav bar
 
 /**************************************
- * Navigation animations for full screen and mobile
+ * Navigation animations for full 
+ * screen and mobile with burger menu
  * 
- * 
- */
-
+ **************************************/
 // burger animation toggle
 const burger = document.getElementsByTagName('nav')[0];
 document.querySelector("#nav-toggle")
@@ -18,6 +36,7 @@ document.querySelector("#nav-toggle")
         this.classList.toggle("active");
         burger.classList.toggle('visibleNav');
     });
+
 
 //reset the burger menu to default when open and resizing window
 let width;
@@ -27,18 +46,15 @@ window.onresize = window.onload = function() {
         document.querySelector("#nav-toggle").classList.remove("active");
         burger.classList.add('visibleNav');
     }
+    videoHeight = (document.getElementsByClassName('video-wrap')[0].clientHeight) - 80;
 }
-
-//toggle animated nav when scrolling vertically
-let scrollHeight;
-let animatedHeader = document.getElementsByTagName('header')[0];
 window.onscroll = window.onload = function() {
     scrollHeight = this.scrollY;
-    if (scrollHeight > 100) {
+    if (scrollHeight > videoHeight) {
         animatedHeader.classList.add('verticalAnimate');
     }
-    if (scrollHeight > 900 || scrollHeight < 100) {
+    if (scrollHeight < videoHeight) {
         animatedHeader.classList.remove('verticalAnimate');
     }
-    console.log(scrollHeight);
+
 }
