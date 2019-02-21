3c2b3f34527d2393b598383a7c6f6ae72eee809a
pmarik <pmarik@uwalumni.com>
2019-02-20 18:02:58 -0600

    updated videos page infinite scroll test 1

---
+++ src/imageSlider.js
@@ -0,0 +1,54 @@
+/**************************************
+ * Image slider
+ * 
+ * Used on the Videos Page for mobile
+ * navigation to select video playlist
+ **************************************/
+export function imgSlider() {
+    let sliderImages = document.querySelectorAll('.slide');
+    let arrowLeft = document.querySelector('#arrow-left');
+    let arrowRight = document.querySelector('#arrow-right');
+    let current = 0;
+
+
+
+    function reset() {
+        for (let i = 0; i < sliderImages.length; i++) {
+            sliderImages[i].style.display = 'none';
+        }
+    }
+
+
+
+    function startSlide() {
+        reset();
+        sliderImages[0].style.display = 'block';
+    }
+
+    function slideLeft() {
+        reset();
+        sliderImages[current - 1].style.display = 'block';
+        current--;
+    }
+
+    function slideRight() {
+        reset();
+        sliderImages[current + 1].style.display = 'block';
+        current++;
+    }
+
+    arrowLeft.addEventListener('click', function() {
+        if (current === 0) {
+            current = sliderImages.length;
+        }
+        slideLeft();
+    });
+
+    arrowRight.addEventListener('click', function() {
+        if (current === sliderImages.length - 1) {
+            current = -1;
+        }
+        slideRight();
+    });
+    startSlide();
+}
